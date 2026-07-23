46a1aa632fce0fb4e66547dde91b919459e8ac9d
Conte-caolei <caolei3218332661@qq.com>
2026-07-23 16:39:25 +0800

    add scripts/SCNet/exchange.sh

---
+++ scripts/SCNet/exchange.sh
@@ -0,0 +1,95 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=SCNet
+# d state 2
+python -u run.py \
+  --is_training 1 \
+  --task_name long_term_forecast \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id exchange_96_96 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 96 \
+  --enc_in 8 \
+  --patch_sizes 96 48 24 12 6 \
+  --d_model 256 \
+  --e_layers 5 \
+  --des 'Exp' \
+  --learning_rate 0.0001 \
+  --train_epochs 20\
+  --d_ff 256 \
+  --itr 1 \
+  --batch_size 16 \
+  --result_file result_long_term_forecast_exchange.txt
+
+python -u run.py \
+  --is_training 1 \
+  --task_name long_term_forecast \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id exchange_96_192 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 192 \
+  --enc_in 8 \
+  --patch_sizes 96 48 24 12 6 \
+  --d_model 256 \
+  --e_layers 5 \
+  --des 'Exp' \
+  --learning_rate 0.0001 \
+  --train_epochs 20\
+  --d_ff 256 \
+  --itr 1 \
+  --batch_size 16 \
+  --result_file result_long_term_forecast_exchange.txt
+
+python -u run.py \
+  --is_training 1 \
+  --task_name long_term_forecast \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id exchange_96_336 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 336 \
+  --enc_in 8 \
+  --patch_sizes 96 48 24 12 6 \
+  --d_model 256 \
+  --e_layers 5 \
+  --des 'Exp' \
+  --learning_rate 0.0001 \
+  --train_epochs 20\
+  --d_ff 256 \
+  --itr 1 \
+  --batch_size 16 \
+  --result_file result_long_term_forecast_exchange.txt
+
+python -u run.py \
+  --is_training 1 \
+  --task_name long_term_forecast \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id exchange_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 720 \
+  --enc_in 8 \
+  --patch_sizes 96 48 24 12 6 \
+  --d_model 256 \
+  --e_layers 5 \
+  --des 'Exp' \
+  --learning_rate 0.0001 \
+  --train_epochs 20\
+  --d_ff 256 \
+  --itr 1 \
+  --batch_size 16 \
+  --result_file result_long_term_forecast_exchange.txt
